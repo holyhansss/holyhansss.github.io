9307b67bbdeff6f5bbbd2f3bf5407ffa353d4cb1
Sungwon Han <holyhansss@gmail.com>
2021-12-07 16:57:11 +0900

    bio description update

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -14,7 +14,7 @@ module.exports = {
     name: `한성원`,
     bio: {
       role: `개발자`,
-      description: ['사람에 가치를 두는', '능동적으로 일하는', '이로운 것을 만드는'],
+      description: ['새로운 것에 도전하는', '능동적으로 일하는', '배워서 남주는'],
       thumbnail: 'sample.png', // Path to the image in the 'asset' folder
     },
     social: {
