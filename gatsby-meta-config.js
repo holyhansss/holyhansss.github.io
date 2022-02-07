a11ea98b57ed1edac01535dfd75a2e4b9b76ea73
Sungwon Han <holyhansss@gmail.com>
2022-02-07 18:59:41 +0900

    project metadata testing

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -71,18 +71,18 @@ module.exports = {
       },
       // ========================================================
       // ========================================================
-      // {
-      //   title: '개발 블로그 테마 개발',
-      //   description:
-      //     '개발 블로그를 운영하는 기간이 조금씩 늘어나고 점점 많은 생각과 경험이 블로그에 쌓아가면서 제 이야기를 담고 있는 블로그를 직접 만들어보고 싶게 되었습니다. 그동안 여러 개발 블로그를 보면서 좋았던 부분과 불편했던 부분들을 바탕으로 레퍼런스를 참고하여 직접 블로그 테마를 만들게 되었습니다.',
-      //   techStack: ['gatsby', 'react'],
-      //   thumbnailUrl: 'blog.png',
-      //   links: {
-      //     post: '/gatsby-starter-zoomkoding-introduction',
-      //     github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
-      //     demo: 'https://www.zoomkoding.com',
-      //   },
-      // },
+      {
+        title: '개발 블로그 테마 개발',
+        description:
+          '한동대학교 ICT 창업학부의 Projects관련',
+        techStack: ['react','firebase'],
+        thumbnailUrl: 'blog.png',
+        links: {
+          post: '/',
+          github: 'https://github.com/holyhansss',
+          demo: 'https://github.com/holyhansss',
+        },
+      },
     ],
   },
 };
